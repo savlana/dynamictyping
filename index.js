b645748ca493035f932c223872e95d6ee14f0644
savlana <108239589+savlana@users.noreply.github.com>
2022-07-17 15:41:17 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let age = 25;
+age = "18"
+console.log(age, typeof age);
+alert(age);
+
+let surname = "Smith";
+surname = 21;
+console.log(surname, typeof surname);
+alert(surname);
+
+let loveSummer = false;
+loveSummer = null;
+console.log("love summer", loveSummer);
+alert(loveSummer);
+
+
+
+
